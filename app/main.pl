3cd89bf2fb1600d09e9aa6f5da8d8831485a8e9a
Ailton Aires <ailtonaires5@gmail.com>
2024-07-08 18:51:38 -0300

    Adicionando base de dados so supermercado.

---
+++ app/main.pl
@@ -0,0 +1,37 @@
+% Supermercados
+supermercado(mercado1, 5).  % Nome e distância em quilômetros (ida)
+supermercado(mercado2, 10).
+supermercado(mercado3, 3).
+supermercado(mercado4, 8).
+supermercado(mercado5, 15).
+
+% Produtos e preços em cada supermercado
+produto(mercado1, arroz, 5.00).
+produto(mercado1, feijao, 4.00).
+produto(mercado1, leite, 3.00).
+produto(mercado1, cafe, 7.00).
+
+produto(mercado2, arroz, 4.50).
+produto(mercado2, feijao, 3.50).
+produto(mercado2, leite, 3.50).
+produto(mercado2, carne, 24.00).
+produto(mercado2, pao, 5.50).
+
+produto(mercado3, arroz, 5.50).
+produto(mercado3, feijao, 4.50).
+produto(mercado3, leite, 2.50).
+produto(mercado3, carne, 26.00).
+produto(mercado3, pao, 6.50).
+
+produto(mercado4, arroz, 4.80).
+produto(mercado4, feijao, 3.80).
+produto(mercado4, leite, 3.20).
+produto(mercado4, carne, 25.50).
+produto(mercado4, pao, 6.20).
+
+produto(mercado5, arroz, 4.30).
+produto(mercado5, feijao, 3.30).
+produto(mercado5, leite, 3.10).
+produto(mercado5, carne, 24.50).
+produto(mercado5, pao, 5.80).
+

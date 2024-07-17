1a84bd3a7571b9043ea8be5cf0135fe7fd6784cc
Ailton Aires <ailtonaires5@gmail.com>
2024-07-17 19:41:20 -0300

    Aumentando a base de supermercados, receitas e produtos

--- app/receitas.pl
+++ app/receitas.pl
@@ -24,11 +24,37 @@ sugerir_receitas(IngredientesDisponiveis, ReceitasPossiveis) :-
 % Define as receitas e seus ingredientes
 % receita(str:'receita', list:[ingredientes, ...])
 :- dynamic receita/2.
+
 receita('Omelete', ['ovos', 'sal', 'oleo', 'cebola']).
 receita('Salada de Tomate', ['tomate', 'sal', 'azeite', 'cebola']).
 receita('Massa com Molho', ['massa', 'tomate', 'sal', 'azeite']).
 receita('Bolo de Chocolate', ['farinha', 'ovos', 'acucar', 'chocolate', 'manteiga']).
 receita('Sopa de Legumes', ['cenoura', 'batata', 'cebola', 'agua', 'sal']).
+
+receita('Frango Grelhado', ['frango', 'sal', 'azeite', 'alho']).
+receita('Arroz de Carreteiro', ['arroz', 'carne seca', 'cebola', 'pimentao', 'alho']).
+receita('Feijoada', ['feijao preto', 'carne seca', 'linguica', 'cebola', 'alho']).
+receita('Moqueca de Peixe', ['peixe', 'tomate', 'pimentao', 'leite de coco', 'azeite de dendê']).
+receita('Pão de Queijo', ['polvilho doce', 'queijo minas', 'ovos', 'leite', 'oleo']).
+
+receita('Brigadeiro', ['leite condensado', 'chocolate em pó', 'manteiga', 'granulado']).
+receita('Quiche de Alho-Poró', ['massa folhada', 'alho-poró', 'ovos', 'creme de leite', 'queijo']).
+receita('Lasanha', ['massa de lasanha', 'carne moída', 'tomate', 'queijo', 'molho bechamel']).
+receita('Tapioca', ['goma de tapioca', 'sal', 'queijo', 'presunto']).
+receita('Farofa', ['farinha de mandioca', 'cebola', 'alho', 'manteiga', 'sal']).
+
+receita('Torta de Frango', ['massa de torta', 'frango desfiado', 'milho', 'ervilha', 'creme de leite']).
+receita('Coxinha', ['frango desfiado', 'farinha de trigo', 'batata', 'caldo de galinha', 'farinha de rosca']).
+receita('Beijinho', ['leite condensado', 'coco ralado', 'manteiga', 'açúcar']).
+receita('Bolinho de Bacalhau', ['bacalhau desfiado', 'batata', 'ovos', 'salsa', 'cebola']).
+receita('Mousse de Maracujá', ['maracujá', 'leite condensado', 'creme de leite', 'gelatina']).
+
+receita('Escondidinho de Carne', ['carne moída', 'mandioca', 'cebola', 'alho', 'queijo parmesão']).
+receita('Risoto de Cogumelos', ['arroz arbóreo', 'cogumelos', 'caldo de legumes', 'cebola', 'queijo parmesão']).
+receita('Panqueca', ['farinha de trigo', 'ovos', 'leite', 'sal', 'recheio a gosto']).
+receita('Cuscuz Paulista', ['farinha de milho', 'tomate', 'palmito', 'ervilha', 'sardinha']).
+receita('Vatapá', ['pão', 'leite de coco', 'azeite de dendê', 'camarão', 'amendoim']).
+
 
 :- dynamic selecionado/1.
 selecionado('Omelete').
